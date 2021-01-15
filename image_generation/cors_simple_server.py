50063c8999782f6c6f41a7e45e19ee477e519bdc
Matt Campbell <48925+mattc@users.noreply.github.com>
2021-01-14 20:42:29 -0500

    Support Python 2 & 3 in image generation.

--- image_generation/cors_simple_server.py
+++ image_generation/cors_simple_server.py
@@ -1,11 +1,17 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+try:
+    from http.server import HTTPServer, SimpleHTTPRequestHandler, test as test_orig
+    import sys
+    def test (*args):
+        test_orig(*args, port=int(sys.argv[1]))
+except ImportError: # Python 2
+    from BaseHTTPServer import HTTPServer, test
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-from SimpleHTTPServer import SimpleHTTPRequestHandler, test
-
-class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
-    def end_headers(self):
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    test(HandlerClass=CORSHTTPRequestHandler)
+    test(CORSRequestHandler, HTTPServer)
